8be6d2a5043241e711e40ef196d4922e8e9601de
Brandon <brandon@nova7.lab304.org>
2012-02-10 15:00:33 -0500

    added EastWalker robot

---
+++ src/org/lab304/robots/tests/EastWalkerTest.java
@@ -0,0 +1,38 @@
+package org.lab304.robots.tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.lab304.robots.bots.Robot;
+import org.lab304.robots.bots.impl.EastWalker;
+import org.lab304.robots.location.Location;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Class description.
+ */
+public class EastWalkerTest {
+    Robot eastWalkingRobot;
+
+    @Before
+    public void setUp() throws Exception {
+        eastWalkingRobot = new EastWalker();
+    }
+
+    @Test
+    public void testDetermineNextLocation() throws Exception {
+
+        Location startLocation = new Location(1, 1);
+        Location expectedLocation = new Location(1, 2);
+        eastWalkingRobot.setLocation(startLocation);
+
+        // the eastWalkingRobot is at the start location
+        assertEquals(startLocation, eastWalkingRobot.getLocation());
+
+        // get the eastWalkingRobot determined location
+        Location determinedLocation = eastWalkingRobot.determineNextLocation();
+
+        // did the eastWalkingRobot determine the expected location
+        assertEquals(expectedLocation, determinedLocation);
+    }
+}
